bbe6cfef144a98a9b6ae6b2525272d818ea9f26d
joaoafonso2026957535 <a2026953575@teiacoltec.org>
2026-05-31 16:02:16 -0300

    Criação da estrutura inicial do projeto

---
+++ codigo/calculadora.c
@@ -0,0 +1,16 @@
+/*
+Projeto Final - Laboratório de Programação
+
+Calculadora Científica em C
+
+Autor: João Afonso
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Projeto iniciado!\n");
+
+    return 0;
+}
